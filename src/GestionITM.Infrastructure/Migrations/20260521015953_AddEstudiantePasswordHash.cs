50b1b19583531b77615f0e7876743e6b2947ee76
Santi <santicorapollo@gmail.com>
2026-05-20 21:29:42 -0500

    Pre1

---
+++ src/GestionITM.Infrastructure/Migrations/20260521015953_AddEstudiantePasswordHash.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionITM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstudiantePasswordHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Estudiantes",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Estudiantes");
+        }
+    }
+}
